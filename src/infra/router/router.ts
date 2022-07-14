bdf157e1fa9461a07844690788ae6f3a50079f62
masashimizuno <masajene8@gmail.com>
2022-07-14 09:39:26 +0900

    fix router for di

--- src/infra/router/router.ts
+++ src/infra/router/router.ts
@@ -4,19 +4,17 @@ import UserHandler from '../../adapter/handler/user_handler'
 import { UserUseCaseImpl } from '../../domain/interactor/user_usecase'
 import { UserRepositoryImpl } from '../../adapter/gateway/user_repository'
 import { MySQLDataSource } from '../database/mysql_datasource'
+import { userContainer } from '../../di/inversify.config'
+import { UserUseCase } from '../../domain/usecase/user_usecase'
+import { TYPES } from '../../di/inversify.types'
 
 const router = express()
 router.use(express.json())
 router.use(morgan('combined'))
 
-const userHandler = UserHandler(
-  new UserUseCaseImpl(new UserRepositoryImpl(new MySQLDataSource()))
-)
+const userDI = userContainer().get<UserUseCase>(TYPES.UserUseCase)
+const userHandler = UserHandler(userDI)
 
-router.use('/user', userHandler)
-router.use('/user', userHandler)
-router.use('/user', userHandler)
-router.use('/user', userHandler)
 router.use('/user', userHandler)
 
 export default router
